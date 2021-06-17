8cb6bc0927feafaa3c5543f48bc0835541999985
NIHILncunia <nihil_ncunia@naver.com>
2021-06-17 16:32:25 +0900

    2021 0617 next 11에 맞게 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   plugins: [
     'import',
+    'react',
     'react-hooks',
   ],
   rules: {
--- next.config.js
+++ next.config.js
@@ -11,4 +11,7 @@ module.exports = {
     }
     return config;
   },
+  eslint: {
+    dirs: [],
+  },
 };
